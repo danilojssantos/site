93237a38c83f78242d753eb669ef185468d01713
danilojssantos <danilojssantos1@gmail.com>
2019-08-29 22:50:12 -0300

    inner join

--- app/sts/Models/StsPaginas.php
+++ app/sts/Models/StsPaginas.php
@@ -17,14 +17,17 @@ class StsPaginas
         $this->UrlController = (string) $UrlController;
         $listar = new \Sts\Models\helper\StsRead();
 
-       $listar->fullRead('SELECT id FROM sts_paginas
-                WHERE sts_situacaos_pg_id =:sts_situacaos_pg_id
-                AND controller =:controller
-                LIMIT :limit', "sts_situacaos_pg_id=1&controller={$this->UrlController}&limit=1");
+        $listar->fullRead('SELECT pg.id,
+        tpg.tipo tipo_tpg
+        FROM sts_paginas pg            
+        INNER JOIN sts_tps_pgs tpg ON tpg.id=pg.sts_tps_pg_id
+        WHERE pg.sts_situacaos_pg_id =:sts_situacaos_pg_id
+        AND pg.controller =:controller
+        LIMIT :limit', "sts_situacaos_pg_id=1&controller={$this->UrlController}&limit=1");
         
         $this->Resultado = $listar->getResultado();
-        //echo "<br><br><br>";
-       // var_dump($this->Resultado); 
+        echo "<br><br><br>";
+       var_dump($this->Resultado); 
         return $this->Resultado;       
     }
 
