19a6d3f6bbeacffe8b1bed5c1364db62104b8c95
danilojssantos <danilojssantos1@gmail.com>
2019-10-05 20:54:16 -0300

    classe generica para fazer delete

---
+++ adm/app/adms/Models/helper/AdmsDelete.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\adms\Models\helper;
+
+if (!defined('URL')) {
+    header("Location: /");
+    exit();
+}
+
+
+class AdmsDelete extends AdmsConn
+{
+
+    private $Tabela;
+    private $Termos;
+    private $Values;
+    private $Resultado;
+    private $Query;
+    private $Conn;
+
+    function getResultado()
+    {
+        return $this->Resultado;
+    }
+
+    public function exeDelete($Tabela, $Termos, $ParseString)
+    {
+        $this->Tabela = (string) $Tabela;
+        $this->Termos = (string) $Termos;
+        parse_str($ParseString, $this->Values);
+
+        $this->executarIntrucao();
+    }
+
+    private function executarIntrucao()
+    {
+        $this->Query = "DELETE FROM {$this->Tabela} {$this->Termos}";
+        $this->conexao();
+        try {
+            $this->Query->execute($this->Query);
+            $this->Resultado = true;
+        } catch (Exception $ex) {
+            $this->Resultado = false;
+        }
+    }
+
+    private function conexao()
+    {
+        $this->Conn = parent::getConn();
+        $this->Query = $this->Conn->prepare($this->Query);
+    }
+
+}
